258f839cc04c326f71e09b0ac801375b4c957878
Carlos <cralosmanuel@gail.com>
2016-07-10 00:24:46 -0400

    Creando estructura del proyecto

--- src/Main/Main/urls.py
+++ src/Main/Main/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'SistemaPedido.views.home', name='home'),
+    # url(r'^$', 'Main.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
